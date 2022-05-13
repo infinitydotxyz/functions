ef4a5e459222d4c765d058440124d64fc7970585
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-05-13 16:20:17 -0500

    changed numTokens constraint to use an equality operator instead of relational

--- src/orders/constraints/num-tokens-constraint.ts
+++ src/orders/constraints/num-tokens-constraint.ts
@@ -4,27 +4,14 @@ import { OrderItemConstraint } from './order-item-constraint.abstract';
 export class OrderItemNumTokensConstraint extends OrderItemConstraint {
   protected score = 0;
 
-  private get isNumTokensUpperBound(): boolean {
-    return this.firestoreOrderItem.isSellOrder ? true : false;
-  }
-
   protected isConstraintSatisfied(orderItem: FirestoreOrderItem): boolean {
-    if (this.isNumTokensUpperBound) {
-      return orderItem.numTokens <= this.firestoreOrderItem.numTokens;
-    }
-    return orderItem.numTokens >= this.component.firestoreOrderItem.numTokens;
+    return orderItem.numTokens == this.firestoreOrderItem.numTokens;
   }
 
   protected addConstraintToQuery(
     query: FirebaseFirestore.Query<FirestoreOrderItem>
   ): FirebaseFirestore.Query<FirestoreOrderItem> {
-    if (this instanceof this.firestoreQueryOrderByConstraint) {
-      if (this.isNumTokensUpperBound) {
-        return query.where('numTokens', '<=', this.firestoreOrderItem.numTokens);
-      }
-      return query.where('numTokens', '>=', this.firestoreOrderItem.numTokens);
-    }
-    return query; // cannot add this constraint and startAfterTime
+    return query.where('numTokens', '==', this.firestoreOrderItem.numTokens);
   }
 
   addOrderByToQuery(
@@ -34,13 +21,12 @@ export class OrderItemNumTokensConstraint extends OrderItemConstraint {
     query: FirebaseFirestore.Query<FirestoreOrderItem>;
     getStartAfter: (item: FirestoreOrderItem) => (string | number)[];
   } {
-    const defaultOrderDirection = this.isNumTokensUpperBound ? OrderDirection.Descending : OrderDirection.Ascending;
+    const defaultOrderDirection = OrderDirection.Descending;
     query = query
-      .orderBy('numTokens', orderDirection ?? defaultOrderDirection)
       .orderBy('collectionAddress', orderDirection ?? defaultOrderDirection);
     return {
       query,
-      getStartAfter: (item: FirestoreOrderItem) => [item.numTokens, item.collectionAddress]
+      getStartAfter: (item: FirestoreOrderItem) => [item.collectionAddress]
     };
   }
 }

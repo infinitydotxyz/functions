4dcdf412e43f8d661ba8f8a1e9184cbf59a45c5e
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-06-21 13:11:30 -0500

    get possible matches and connect order items that are matches

--- src/graph/orders-graph.ts
+++ src/graph/orders-graph.ts
@@ -1,5 +1,4 @@
 import { Graph } from './graph';
-import { OrderItem as IOrder } from '../orders/orders.types';
 import { Order } from '../orders/order';
 import { Node } from './node';
 import { getDb } from '../firestore';
@@ -16,33 +15,48 @@ export class OrdersGraph extends Graph<Order> {
   }
 
   async buildGraph() {
-    const orderItems = await this.root.data.getOrderItems();
     const db = getDb();
     const orderIds = new Set<string>();
 
-    // const orderNodes: OrderNodeCollection[] = [];
-
     const rootOrderNode = await this.getOrderNode(this.root.data.firestoreOrder);
+    const orderNodes: OrderNodeCollection[] = [];
 
-    // for(const orderItemNode of rootOrderNode.nodes) {
-    //   orderItemNode.
-    // }
-
-    for (const orderItem of orderItems) {
-      const matches = orderItem.getPossibleMatches();
-      for await (const match of matches) {
-        if (!orderIds.has(match.id)) {
+    /**
+     * get all orders that can fulfill at least a subset of order
+     */
+    for(const orderItemNode of rootOrderNode.nodes) {
+      const possibleMatches = orderItemNode.data.getPossibleMatches();
+      for await (const match of possibleMatches) {
+        if (!orderIds.has(match.id) && orderItemNode.data.isMatch(match)) {
+          orderIds.add(match.id);
           const orderItem = new OrderItem(match, db);
           const firestoreOrder = (await orderItem.orderRef.get()).data();
           if (firestoreOrder) {
             const orderNode = await this.getOrderNode(firestoreOrder);
-            // orderNodes.push(orderNode);
+            orderNodes.push(orderNode);
+          }
+        }
+      }
+    };
+
+    /**
+     * sort order nodes by increasing start time
+     */
+    orderNodes.sort((a, b) => a.data.order.firestoreOrder.startTimeMs - b.data.order.firestoreOrder.startTimeMs);
+
+    /**
+     * build edges between order items that can fulfill each other
+     */
+    for(const orderNode of orderNodes) {
+      for(const orderItemNode of orderNode.nodes) {
+        for(const rootOrderItemNode of rootOrderNode.nodes) {
+          if(rootOrderItemNode.data.isMatch(orderItemNode.data.firestoreOrderItem)) {
+            const edge = new Edge();
+            edge.link(rootOrderItemNode, orderItemNode);
           }
         }
       }
     }
-
-    ;
   }
 
   

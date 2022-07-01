44459d0141c7128ac03d5647eea415ee94bb68ad
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-07-01 10:58:14 -0500

    added order side constraint unit tests

---
+++ src/orders/constraints/order-side-constraint.spec.ts
@@ -0,0 +1,26 @@
+
+
+import { getOrderItem } from './chain-id-constraint.spec';
+import { constraints } from './constraint.types';
+import { OrderItemOrderSideConstraint } from './order-side-constraint';
+
+describe('order side constraint', () => {
+  it('only matches order items of the opposite order side', () => {
+
+    const buy = getOrderItem({ isSellOrder: false });
+    const sell = getOrderItem({ isSellOrder: true });
+
+    const constraintBuy = new OrderItemOrderSideConstraint(buy);
+    const constraintSell = new OrderItemOrderSideConstraint(sell);
+
+    expect(constraintBuy.isMatch(sell.firestoreOrderItem)).toBe(true);
+    expect(constraintBuy.isMatch(buy.firestoreOrderItem)).toBe(false);
+    expect(constraintSell.isMatch(buy.firestoreOrderItem)).toBe(true);
+    expect(constraintSell.isMatch(sell.firestoreOrderItem)).toBe(false);
+  });
+
+  it('constraint is included', () => {
+    const isIncluded = constraints.some(item => item === OrderItemOrderSideConstraint);
+    expect(isIncluded).toBe(true);
+  });
+});

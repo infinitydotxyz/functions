5889e5829e148a4d667152f57918f0196db851a2
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-07-01 11:16:54 -0500

    added price constraint unit tests

---
+++ src/orders/constraints/price-constraint.spec.ts
@@ -0,0 +1,100 @@
+import { getOrderItem } from './chain-id-constraint.spec';
+import { constraints } from './constraint.types';
+import { OrderItemPriceConstraint } from './price-constraint';
+
+describe('price constraint', () => {
+  it('matches order items with the same start time, end time, and prices', () => {
+    const startTimeMs = Date.now();
+    const endTimeMs = startTimeMs + 3_600_000;
+    const desc = { startPriceEth: 1, endPriceEth: 2, startTimeMs, endTimeMs };
+
+    const sell = getOrderItem({...desc, isSellOrder: true});
+    const buy = getOrderItem ({...desc, isSellOrder: false});
+
+    const sellConstraint = new OrderItemPriceConstraint(sell);
+    const buyConstraint = new OrderItemPriceConstraint(buy);
+
+    expect(sellConstraint.isMatch(buy.firestoreOrderItem)).toBe(true);
+    expect(buyConstraint.isMatch(sell.firestoreOrderItem)).toBe(true);
+  });
+
+  it('matches order items where the buy price is greater than the sell price', () => {
+    const startTimeMs = Date.now();
+    const endTimeMs = startTimeMs + 3_600_000;
+    const desc = { startPriceEth: 1, endPriceEth: 2, startTimeMs, endTimeMs };
+
+    const sell = getOrderItem({...desc, isSellOrder: true});
+    const buy = getOrderItem ({...desc, isSellOrder: false, startPriceEth: 3, endPriceEth: 4});
+
+    const sellConstraint = new OrderItemPriceConstraint(sell);
+    const buyConstraint = new OrderItemPriceConstraint(buy);
+
+    expect(sellConstraint.isMatch(buy.firestoreOrderItem)).toBe(true);
+    expect(buyConstraint.isMatch(sell.firestoreOrderItem)).toBe(true);
+  });
+
+  it('doesn\'t matches order items where the sell price is greater than the buy price', () => {
+    const startTimeMs = Date.now();
+    const endTimeMs = startTimeMs + 3_600_000;
+    const desc = { startPriceEth: 1, endPriceEth: 2, startTimeMs, endTimeMs };
+
+    const sell = getOrderItem({...desc, isSellOrder: true, startPriceEth: 3, endPriceEth: 4});
+    const buy = getOrderItem ({...desc, isSellOrder: false});
+
+    const sellConstraint = new OrderItemPriceConstraint(sell);
+    const buyConstraint = new OrderItemPriceConstraint(buy);
+
+    expect(sellConstraint.isMatch(buy.firestoreOrderItem)).toBe(false);
+    expect(buyConstraint.isMatch(sell.firestoreOrderItem)).toBe(false);
+  });
+
+  it('doesn\'t matches order items where the time intervals don\'t overlap', () => {
+    const startTimeMs = Date.now();
+    const endTimeMs = startTimeMs + 3_600_000;
+    const desc = { startPriceEth: 1, endPriceEth: 2, startTimeMs, endTimeMs };
+
+    const sell = getOrderItem({...desc, isSellOrder: true});
+    const buy = getOrderItem ({...desc, isSellOrder: false, startTimeMs: endTimeMs + 1, endTimeMs: endTimeMs + 3_600_000});
+
+    const sellConstraint = new OrderItemPriceConstraint(sell);
+    const buyConstraint = new OrderItemPriceConstraint(buy);
+
+    expect(sellConstraint.isMatch(buy.firestoreOrderItem)).toBe(false);
+    expect(buyConstraint.isMatch(sell.firestoreOrderItem)).toBe(false);
+  });
+
+  it('matches order items where the sell order decreases to intersect with the buy order', () => {
+    const startTimeMs = Date.now();
+    const endTimeMs = startTimeMs + 3_600_000;
+    const desc = { startPriceEth: 2, endPriceEth: 2, startTimeMs, endTimeMs };
+
+    const sell = getOrderItem({...desc, isSellOrder: true, startPriceEth: 4, endPriceEth: 1});
+    const buy = getOrderItem ({...desc, isSellOrder: false });
+
+    const sellConstraint = new OrderItemPriceConstraint(sell);
+    const buyConstraint = new OrderItemPriceConstraint(buy);
+
+    expect(sellConstraint.isMatch(buy.firestoreOrderItem)).toBe(true);
+    expect(buyConstraint.isMatch(sell.firestoreOrderItem)).toBe(true);
+  });
+
+  it('matches order items where the buy order increases to intersect with the buy order', () => {
+    const startTimeMs = Date.now();
+    const endTimeMs = startTimeMs + 3_600_000;
+    const desc = { startPriceEth: 2, endPriceEth: 2, startTimeMs, endTimeMs };
+
+    const sell = getOrderItem({...desc, isSellOrder: true });
+    const buy = getOrderItem ({...desc, isSellOrder: false, startPriceEth: 1, endPriceEth: 3 });
+
+    const sellConstraint = new OrderItemPriceConstraint(sell);
+    const buyConstraint = new OrderItemPriceConstraint(buy);
+
+    expect(sellConstraint.isMatch(buy.firestoreOrderItem)).toBe(true);
+    expect(buyConstraint.isMatch(sell.firestoreOrderItem)).toBe(true);
+  });
+
+  it('constraint is included', () => {
+    const isIncluded = constraints.some((item) => item === OrderItemPriceConstraint);
+    expect(isIncluded).toBe(true);
+  });
+});

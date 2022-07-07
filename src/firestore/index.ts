8f79f3660bb14fd314f05a0ad01d9d784be2e866
nneverlander <findadit@gmail.com>
2022-07-07 05:38:57 +0200

    minor

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -1,5 +1,4 @@
 import admin, { ServiceAccount } from 'firebase-admin';
-// todo adi update this for prod
 import * as serviceAccount from '../creds/nftc-dev-firebase-creds.json';
 
 let db: FirebaseFirestore.Firestore;

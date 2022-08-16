917d6af6757b8197e720fa7b6921c840a32e6e1c
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-08-16 08:32:53 -0500

    write vote/unvote events to the feed

---
+++ src/functions/update-curation-feed/create-feed-event.ts
@@ -0,0 +1,108 @@
+import {
+  Collection,
+  CurationLedgerEvent,
+  CurationLedgerSale,
+  CurationVotesAdded,
+  CurationVotesRemoved,
+  EventType,
+  InfinityLinkType,
+  UserVoteEvent,
+  UserVoteRemovedEvent
+} from '@infinityxyz/lib/types/core';
+import { UserProfileDto } from '@infinityxyz/lib/types/dto/user/user-profile.dto';
+import { firestoreConstants, getInfinityLink } from '@infinityxyz/lib/utils';
+
+export type CurationLedgerEvents = CurationLedgerSale | CurationVotesAdded | CurationVotesRemoved;
+
+export async function createFeedEventForLedgerEvent(
+  ledgerEventRef: FirebaseFirestore.DocumentReference<CurationLedgerEvents>
+) {
+  const db = ledgerEventRef.firestore;
+  await db.runTransaction(async (txn) => {
+    const feedRef = db.collection(firestoreConstants.FEED_COLL);
+    const ledgerEventSnap = await txn.get(ledgerEventRef);
+    const ledgerEvent = ledgerEventSnap.data();
+    if (ledgerEvent && 'isFeedUpdated' in ledgerEvent && ledgerEvent.isFeedUpdated === false) {
+      const userRef = db.collection(firestoreConstants.USERS_COLL).doc(ledgerEvent.userAddress);
+      const collectionRef = ledgerEventRef.parent.parent?.parent.parent;
+      if (!collectionRef) {
+        throw new Error('Collection ref not found');
+      }
+      const collectionSnap = await txn.get(collectionRef);
+      const userProfileSnap = await txn.get(userRef);
+      const userProfile = userProfileSnap.data() as Partial<UserProfileDto>;
+      const collection: Partial<Collection> = collectionSnap.data() ?? {};
+
+      let event: UserVoteEvent | UserVoteRemovedEvent;
+
+      switch (ledgerEvent.discriminator) {
+        case CurationLedgerEvent.VotesAdded: {
+          const voteEvent: UserVoteEvent = {
+            type: EventType.UserVote,
+            votesAdded: ledgerEvent.votes,
+            timestamp: ledgerEvent.timestamp,
+            userAddress: ledgerEvent.userAddress,
+            userUsername: userProfile.username || '',
+            userDisplayName: userProfile.displayName || '',
+            userProfileImage: userProfile.profileImage || '',
+            likes: 0,
+            comments: 0,
+            usersInvolved: [ledgerEvent.userAddress],
+            stakerContractAddress: ledgerEvent.stakerContractAddress,
+            stakerContractChainId: ledgerEvent.stakerContractChainId,
+            chainId: ledgerEvent.collectionChainId,
+            collectionAddress: ledgerEvent.collectionAddress,
+            collectionName: collection?.metadata?.name || '',
+            collectionSlug: collection?.slug || '',
+            collectionProfileImage: collection?.metadata?.profileImage || '',
+            hasBlueCheck: collection.hasBlueCheck || false,
+            internalUrl: getInfinityLink({
+              type: InfinityLinkType.Collection,
+              addressOrSlug: ledgerEvent.collectionAddress,
+              chainId: ledgerEvent.collectionChainId
+            })
+          };
+          event = voteEvent;
+          break;
+        }
+        case CurationLedgerEvent.VotesRemoved: {
+          const voteRemovedEvent: UserVoteRemovedEvent = {
+            type: EventType.UserVoteRemoved,
+            votesRemoved: ledgerEvent.votes,
+            timestamp: ledgerEvent.timestamp,
+            userAddress: ledgerEvent.userAddress,
+            userUsername: userProfile.username || '',
+            userDisplayName: userProfile.displayName || '',
+            userProfileImage: userProfile.profileImage || '',
+            likes: 0,
+            comments: 0,
+            usersInvolved: [ledgerEvent.userAddress],
+            stakerContractAddress: ledgerEvent.stakerContractAddress,
+            stakerContractChainId: ledgerEvent.stakerContractChainId,
+            chainId: ledgerEvent.collectionChainId,
+            collectionAddress: ledgerEvent.collectionAddress,
+            collectionName: collection?.metadata?.name || '',
+            collectionSlug: collection?.slug || '',
+            collectionProfileImage: collection?.metadata?.profileImage || '',
+            hasBlueCheck: collection.hasBlueCheck || false,
+            internalUrl: getInfinityLink({
+              type: InfinityLinkType.Collection,
+              addressOrSlug: ledgerEvent.collectionAddress,
+              chainId: ledgerEvent.collectionChainId
+            })
+          };
+          event = voteRemovedEvent;
+          break;
+        }
+
+        default:
+          throw new Error(`Unhandled event type ${(ledgerEvent as unknown as any)?.discriminator}`);
+      }
+      txn.create(feedRef.doc(), event);
+      const updatedLedgerEvent: Partial<CurationVotesAdded> | Partial<CurationVotesRemoved> = {
+        isFeedUpdated: true
+      };
+      txn.update(ledgerEventRef, updatedLedgerEvent);
+    }
+  });
+}
---
+++ src/functions/update-curation-feed/index.ts
@@ -0,0 +1,42 @@
+import { CurationLedgerSale, CurationVotesAdded, CurationVotesRemoved } from "@infinityxyz/lib/types/core";
+import { firestoreConstants } from "@infinityxyz/lib/utils";
+import { getDb } from "../../firestore";
+import { streamQueryWithRef } from "../../firestore/stream-query";
+import { REGION } from "../../utils/constants";
+import { createFeedEventForLedgerEvent, CurationLedgerEvents } from "./create-feed-event";
+import * as functions from 'firebase-functions';
+
+export const saveLedgerEventsToFeed = functions
+  .region(REGION)
+  .firestore.document(
+    `${firestoreConstants.COLLECTIONS_COLL}/{collectionId}/${firestoreConstants.COLLECTION_CURATION_COLL}/{stakingContractId}/${firestoreConstants.CURATION_LEDGER_COLL}/{ledgerEventId}`
+  )
+  .onCreate(async (snap) => {
+    const ledgerEvent = snap.data() as CurationLedgerSale | CurationVotesAdded | CurationVotesRemoved;
+    if ('isFeedUpdated' in ledgerEvent && !ledgerEvent.isFeedUpdated) {
+      await createFeedEventForLedgerEvent(snap.ref as FirebaseFirestore.DocumentReference<CurationLedgerEvents>);
+    }
+  });
+
+export const backupSaveLedgerEventsToFeed = functions
+  .region(REGION)
+  .runWith({
+    timeoutSeconds: 540
+  })
+  .pubsub.schedule('0,10,20,30,40,50 * * * *')
+  .onRun(async () => {
+    const db = getDb();
+    const tenMin = 1000 * 60 * 10;
+    const curationEventsToAggregate = db
+      .collectionGroup(firestoreConstants.CURATION_LEDGER_COLL)
+      .where('isFeedUpdated', '==', false)
+      .where('updatedAt', '<', Date.now() - tenMin);
+
+    const stream = streamQueryWithRef(curationEventsToAggregate, (item, ref) => [ref], { pageSize: 300 });
+
+    for await (const { ref, data: ledgerEvent } of stream) {
+      if ('isFeedUpdated' in ledgerEvent && !ledgerEvent.isFeedUpdated) {
+        await createFeedEventForLedgerEvent(ref as FirebaseFirestore.DocumentReference<CurationLedgerEvents>);
+      }
+    }
+  });

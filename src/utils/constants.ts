aa43443813d09e3e2c932af283eb61c5b28acbfc
Sleeyax <yourd3veloper@gmail.com>
2022-10-28 19:23:31 +0200

    update webhook URL to infinity sweeps-dev channel

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,4 +5,4 @@ export const REGION = 'us-east1';
 export const ETHER = BigNumber.from(10).pow(18);
 export const GWEI = BigNumber.from(10).pow(9);
 
-export const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1035196393234640966/bnSjl_tCXMN-gOpw8fEA1jgoC2zbPa-tWxdP508PvNmIAibCUaW4-jRIL9UBcOFkwmeC'; // TODO: change this
+export const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1035604415522021477/z67HKspH65Tw2LJJwYTpy5mV5ej22MGAYQilxhaOFWPOTgkmWp1mul5Whca8yPQ1psRt'; // TODO: change this to prod URL

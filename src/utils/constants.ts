ff9e727009d877a442bb841d7fe86cea79d26227
Sleeyax <yourd3veloper@gmail.com>
2022-10-28 23:45:48 +0200

    use prod discord webhook

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,4 +5,4 @@ export const REGION = 'us-east1';
 export const ETHER = BigNumber.from(10).pow(18);
 export const GWEI = BigNumber.from(10).pow(9);
 
-export const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1035604415522021477/z67HKspH65Tw2LJJwYTpy5mV5ej22MGAYQilxhaOFWPOTgkmWp1mul5Whca8yPQ1psRt'; // TODO: change this to prod URL
+export const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1035601491584634990/GlNcNKeo-mNa54EPhrcpGo0Vur99p8oPm95Iumnh49hnpEUj7Hjwa1_VZss36xPdAOMx';

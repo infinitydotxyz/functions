ba31aad26e5aeda0d65cc4c0d87569580691b33d
nneverlander <findadit@gmail.com>
2022-12-28 13:03:46 -0300

    config change to set right firebase credds

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,7 @@ import { ServiceAccount } from 'firebase-admin';
 import { ChainId } from '@infinityxyz/lib/types/core';
 import { trimLowerCase } from '@infinityxyz/lib/utils';
 
-// TODO adi change in release
-import * as serviceAccount from '../creds/nftc-dev-firebase-creds.json';
+import * as serviceAccount from '../creds/nftc-infinity-firebase-creds.json';
 
 const getEnvVariable = (key: string, required = true): string => {
   if (key in process.env && process.env[key] != null && typeof process.env[key] === 'string') {

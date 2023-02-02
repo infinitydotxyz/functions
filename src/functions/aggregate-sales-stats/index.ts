b3dc26e3acfdaed985aeabed32f12dec6605aa87
nneverlander <findadit@gmail.com>
2023-02-02 13:58:53 -0300

    suggested changes

--- src/functions/aggregate-sales-stats/index.ts
+++ src/functions/aggregate-sales-stats/index.ts
@@ -5,6 +5,7 @@ import { aggregateSalesStats } from './aggregate-sales';
 
 export const saveSalesToBeAggregated = functions
   .region(config.firebase.region)
+  .runWith({ timeoutSeconds: 55, maxInstances: 1 })
   .pubsub.schedule('every 1 minutes')
   .onRun(async () => {
     await aggregateSalesStats();

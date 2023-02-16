5e8fcbc4092021e99586ef7ee165642851f4fa22
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-02-16 11:09:44 -0600

    trigger reservoir order events based on error code

--- src/scripts/trigger-order-events.ts
+++ src/scripts/trigger-order-events.ts
@@ -2,39 +2,55 @@ import 'module-alias/register';
 
 import { redis } from '@/app-engine/redis';
 import { getDb } from '@/firestore/db';
+import { SupportedCollectionsProvider } from '@/lib/collections/supported-collections-provider';
+import { ErrorCode } from '@/lib/orderbook/errors';
 import {
   JobData,
   TriggerReservoirOrderEventsProcessor
 } from '@/lib/orderbook/process/trigger-reservoir-order-events/trigger-order-events';
+import { ReservoirOrderEventTrigger } from '@/lib/orderbook/process/trigger-reservoir-order-events/trigger-processor';
 
 import { config } from '../config';
 
 async function main() {
   const db = getDb();
-  const id = `trigger-order-events:env:${config.isDev ? 'dev' : 'prod'}`;
+
+  const errorCode = ErrorCode.FailedToGetReservoirOrder;
+
+  const supportedCollectionsProvider = new SupportedCollectionsProvider(db);
+
+  const triggerer = new ReservoirOrderEventTrigger(redis, db, supportedCollectionsProvider, {
+    enableMetrics: false,
+    concurrency: 1,
+    debug: true,
+    attempts: 1
+  });
+
+  const id = `trigger-order-events:env:${config.isDev ? 'dev' : 'prod'}:errorCode:${errorCode}`;
   const processor = new TriggerReservoirOrderEventsProcessor(id, redis, db, {
     enableMetrics: false,
-    concurrency: 32,
+    concurrency: 1,
     debug: true,
     attempts: 1,
     delay: 0
   });
 
-  const numQueries = 32;
+  const numQueries = 1;
 
   const jobs = [];
   for (let queryNum = 0; queryNum < numQueries; queryNum++) {
     const jobData: JobData = {
       id: `${queryNum}`,
       queryNum,
-      numQueries
+      numQueries,
+      errorCode
     };
     jobs.push(jobData);
   }
 
   await processor.add(jobs);
 
-  await processor.run();
+  await Promise.all([triggerer.run(), processor.run()]);
 }
 
 void main();

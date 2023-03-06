9193fcc7c6c3b6f15561f4b6f26e8e0d0bd98777
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-03-06 12:44:25 -0600

    improved throughput

--- src/functions/aggregate-sales-stats/aggregate-sales.ts
+++ src/functions/aggregate-sales-stats/aggregate-sales.ts
@@ -1,3 +1,5 @@
+import PQueue from 'p-queue';
+
 import {
   ChainId,
   CollectionStats,
@@ -12,6 +14,7 @@ import { firestoreConstants, getCollectionDocId } from '@infinityxyz/lib/utils';
 import { getDb } from '@/firestore/db';
 import { streamQueryPageWithRef } from '@/firestore/stream-query';
 import { DocRef, DocSnap } from '@/firestore/types';
+import { partitionArray } from '@/lib/utils';
 
 export async function aggregateSalesStats() {
   const db = getDb();
@@ -19,7 +22,7 @@ export async function aggregateSalesStats() {
     .collection(firestoreConstants.SALES_COLL)
     .where('isAggregated', '==', false) as FirebaseFirestore.Query<NftSale>;
   const unaggregatedSalesStream = streamQueryPageWithRef(unaggregatedSales, (item, ref) => [ref], {
-    pageSize: 150 // ~ 500 / 3 since we write to at most 3 documents per item
+    pageSize: 600
   });
 
   for await (const page of unaggregatedSalesStream) {
@@ -35,64 +38,69 @@ export async function aggregateSalesStats() {
       }
     );
     const collectionStatsSnaps = (await db.getAll(...collections)) as DocSnap<CollectionStats>[];
-
-    const sortedItems = page.sort((itemA, itemB) => {
-      return itemA.data.price - itemB.data.price;
-    });
     const updatedCollectionStats = new Set();
 
-    // eslint-disable-next-line @typescript-eslint/require-await
-    await db.runTransaction(async (tx) => {
-      for (const { data, ref } of sortedItems) {
-        const saleWithDocId: NftSale & { docId: string; updatedAt: number } = {
-          ...data,
-          docId: ref.id,
-          updatedAt: Date.now()
-        };
+    const chunks = partitionArray(
+      page.sort((itemA, itemB) => {
+        return itemA.data.price - itemB.data.price;
+      }),
+      150 // ~ 500 / 3 since we write to at most 3 documents per item
+    );
 
-        if (saleWithDocId.source === 'flow') {
-          const saleEvent: PreMergedRewardSaleEvent = {
-            ...(saleWithDocId as FlowNftSale & { docId: string; updatedAt: number }),
-            source: saleWithDocId.source,
-            discriminator: RewardEventVariant.Sale,
-            chainId: saleWithDocId.chainId as ChainId,
-            isMerged: false
+    for (const sortedItems of chunks) {
+      // eslint-disable-next-line @typescript-eslint/require-await
+      await db.runTransaction(async (tx) => {
+        for (const { data, ref } of sortedItems) {
+          const saleWithDocId: NftSale & { docId: string; updatedAt: number } = {
+            ...data,
+            docId: ref.id,
+            updatedAt: Date.now()
           };
-          const flowRewardsLedger = db
-            .collection(firestoreConstants.REWARDS_COLL)
-            .doc(saleWithDocId.chainId)
-            .collection('flowRewardsLedger');
-          tx.set(flowRewardsLedger.doc(), saleEvent);
-        }
 
-        const saleUpdate: Pick<NftSale, 'isAggregated'> = {
-          isAggregated: true
-        };
+          if (saleWithDocId.source === 'flow') {
+            const saleEvent: PreMergedRewardSaleEvent = {
+              ...(saleWithDocId as FlowNftSale & { docId: string; updatedAt: number }),
+              source: saleWithDocId.source,
+              discriminator: RewardEventVariant.Sale,
+              chainId: saleWithDocId.chainId as ChainId,
+              isMerged: false
+            };
+            const flowRewardsLedger = db
+              .collection(firestoreConstants.REWARDS_COLL)
+              .doc(saleWithDocId.chainId)
+              .collection('flowRewardsLedger');
+            tx.set(flowRewardsLedger.doc(), saleEvent);
+          }
 
-        const collDocId = getCollectionDocId({
-          collectionAddress: saleWithDocId.collectionAddress,
-          chainId: saleWithDocId.chainId
-        });
+          const saleUpdate: Pick<NftSale, 'isAggregated'> = {
+            isAggregated: true
+          };
 
-        if (!updatedCollectionStats.has(collDocId)) {
-          const collectionStats = collectionStatsSnaps.find((item) => item.ref.path.includes(collDocId));
-          if (collectionStats) {
-            const statsData = collectionStats.data();
-            if (statsData) {
-              const dataToStore: Partial<CollectionStats> = {
-                floorPrice: saleWithDocId.price < statsData.floorPrice ? saleWithDocId.price : statsData.floorPrice,
-                numSales: statsData.numSales + 1,
-                volume: statsData.volume + saleWithDocId.price,
-                updatedAt: Date.now()
-              };
-              tx.set(collectionStats.ref, dataToStore, { merge: true });
-              updatedCollectionStats.add(collDocId);
+          const collDocId = getCollectionDocId({
+            collectionAddress: saleWithDocId.collectionAddress,
+            chainId: saleWithDocId.chainId
+          });
+
+          if (!updatedCollectionStats.has(collDocId)) {
+            const collectionStats = collectionStatsSnaps.find((item) => item.ref.path.includes(collDocId));
+            if (collectionStats) {
+              const statsData = collectionStats.data();
+              if (statsData) {
+                const dataToStore: Partial<CollectionStats> = {
+                  floorPrice: saleWithDocId.price < statsData.floorPrice ? saleWithDocId.price : statsData.floorPrice,
+                  numSales: statsData.numSales + 1,
+                  volume: statsData.volume + saleWithDocId.price,
+                  updatedAt: Date.now()
+                };
+                tx.set(collectionStats.ref, dataToStore, { merge: true });
+                updatedCollectionStats.add(collDocId);
+              }
             }
           }
-        }
 
-        tx.update(ref, saleUpdate);
-      }
-    });
+          tx.update(ref, saleUpdate);
+        }
+      });
+    }
   }
 }

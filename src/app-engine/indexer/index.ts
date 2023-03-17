78c990b8a584178655b17d859eb04d563b40072d
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-03-17 12:17:34 -0500

    added match and take order event queues

--- src/app-engine/indexer/index.ts
+++ src/app-engine/indexer/index.ts
@@ -1,4 +1,26 @@
-import { Queue } from 'bullmq';
 import cron from 'node-cron';
 
-cron.schedule('*/15 * * * * *', async () => {});
+import { Erc20ApprovalEventsQueue } from './erc20-approval';
+import { Erc20TransferEventsQueue } from './erc20-transfer';
+import { Erc721ApprovalEventsQueue } from './erc721-approval';
+import { Erc721ApprovalForAllEventsQueue } from './erc721-approval-for-all';
+import { Erc721TransferEventsQueue } from './erc721-transfer';
+import { ExpirationEventsQueue } from './expiration';
+import { FlowCancelAllEventsQueue } from './flow-cancel-all';
+import { FlowCancelMultipleEventsQueue } from './flow-cancel-multiple';
+import { FlowMatchOrderFulfilledEventsQueue } from './flow-match-order.ts';
+import { FlowTakeOrderFulfilledEventsQueue } from './flow-take-order.ts';
+
+async function startIndexer() {
+  //     Erc20ApprovalEventsQueue,
+  //     Erc20TransferEventsQueue,
+  //     Erc721ApprovalEventsQueue,
+  //     Erc721ApprovalForAllEventsQueue,
+  //     Erc721TransferEventsQueue,
+  //     ExpirationEventsQueue,
+  //     FlowCancelAllEventsQueue,
+  //     FlowCancelMultipleEventsQueue,
+  //     FlowMatchOrderFulfilledEventsQueue,
+  //     FlowTakeOrderFulfilledEventsQueue
+  //   cron.schedule('*/15 * * * * *', async () => {});
+}

058eef1f923db6afb9cd7a06083b91575c776ce3
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-03-20 06:32:41 -0500

    added log

--- src/lib/orderbook/indexer/validate-orders.ts
+++ src/lib/orderbook/indexer/validate-orders.ts
@@ -1,5 +1,3 @@
-import { logger } from 'ethers';
-
 import {
   ChainId,
   OrderApprovalChangeEvent,
@@ -17,6 +15,7 @@ import { BatchHandler } from '@/firestore/batch-handler';
 import { getDb } from '@/firestore/db';
 import { streamQueryWithRef } from '@/firestore/stream-query';
 import { Query } from '@/firestore/types';
+import { logger } from '@/lib/logger';
 import { Erc20ApprovalEventData } from '@/lib/on-chain-events/erc20/erc20-approval';
 import { Erc20TransferEventData } from '@/lib/on-chain-events/erc20/erc20-transfer';
 import { Erc721ApprovalEventData } from '@/lib/on-chain-events/erc721/erc721-approval';
@@ -57,76 +56,82 @@ export async function validateOrders(
       } as OrderEventMetadata & { eventKind: T };
     };
     const chainIdInt = parseInt(orderData.metadata.chainId, 10);
-    let order;
+    let order: Flow.Order | null = null;
     try {
       order = new Flow.Order(chainIdInt, orderData.rawOrder.infinityOrder as Flow.Types.SignedOrder);
     } catch (err) {
       logger.warn('indexer', `Failed to parse order ${err}`);
     }
-    if (order) {
-      const status = await getOrderStatus(order);
-      let statusEvent: OrderApprovalChangeEvent | OrderBalanceChangeEvent | OrderTokenOwnerUpdate;
-      switch (type) {
-        case OrderEventKind.ApprovalChange: {
-          statusEvent = {
-            metadata: getMetadata(OrderEventKind.ApprovalChange),
-            data: {
-              txHash: contractEvent.baseParams.txHash,
-              txTimestamp: contractEvent.baseParams.blockTimestamp,
-              status: status
-            }
-          };
-          break;
-        }
-        case OrderEventKind.BalanceChange: {
-          statusEvent = {
-            metadata: getMetadata(OrderEventKind.BalanceChange),
-            data: {
-              txHash: contractEvent.baseParams.txHash,
-              txTimestamp: contractEvent.baseParams.blockTimestamp,
-              status: status
-            }
-          };
-          break;
-        }
-        case OrderEventKind.TokenOwnerUpdate: {
-          const _contractEvent = contractEvent as ContractEvent<Erc721TransferEventData>;
-          const ownerAddress = _contractEvent.metadata.reorged
-            ? await new Common.Helpers.Erc721(provider, _contractEvent.baseParams.address).getOwner(
-                _contractEvent.event.tokenId
-              )
-            : _contractEvent.event.to;
-          const ownerRef = getDb()
-            .collection('users')
-            .doc(ownerAddress) as FirebaseFirestore.DocumentReference<UserProfileDto>;
-          const owner = await getUserDisplayData(ownerRef);
-          statusEvent = {
-            metadata: getMetadata(OrderEventKind.TokenOwnerUpdate),
-            data: {
-              txHash: contractEvent.baseParams.txHash,
-              txTimestamp: contractEvent.baseParams.blockTimestamp,
-              status: status,
-              token: {
-                address: _contractEvent.baseParams.address,
-                tokenId: _contractEvent.event.tokenId
-              },
-              owner
-            }
-          };
-          break;
-        }
-        default: {
-          throw new Error(`Unhandled event type ${type}`);
-        }
+    const status = await getOrderStatus(order);
+    let statusEvent: OrderApprovalChangeEvent | OrderBalanceChangeEvent | OrderTokenOwnerUpdate;
+    switch (type) {
+      case OrderEventKind.ApprovalChange: {
+        statusEvent = {
+          metadata: getMetadata(OrderEventKind.ApprovalChange),
+          data: {
+            txHash: contractEvent.baseParams.txHash,
+            txTimestamp: contractEvent.baseParams.blockTimestamp,
+            status: status
+          }
+        };
+        break;
       }
-
-      const statusEventRef = orderRef.collection('orderEvents').doc(statusEvent.metadata.id);
-      await batch.addAsync(statusEventRef, statusEvent, { merge: true });
+      case OrderEventKind.BalanceChange: {
+        statusEvent = {
+          metadata: getMetadata(OrderEventKind.BalanceChange),
+          data: {
+            txHash: contractEvent.baseParams.txHash,
+            txTimestamp: contractEvent.baseParams.blockTimestamp,
+            status: status
+          }
+        };
+        break;
+      }
+      case OrderEventKind.TokenOwnerUpdate: {
+        const _contractEvent = contractEvent as ContractEvent<Erc721TransferEventData>;
+        const ownerAddress = _contractEvent.metadata.reorged
+          ? await new Common.Helpers.Erc721(provider, _contractEvent.baseParams.address).getOwner(
+              _contractEvent.event.tokenId
+            )
+          : _contractEvent.event.to;
+        const ownerRef = getDb()
+          .collection('users')
+          .doc(ownerAddress) as FirebaseFirestore.DocumentReference<UserProfileDto>;
+        const owner = await getUserDisplayData(ownerRef);
+        statusEvent = {
+          metadata: getMetadata(OrderEventKind.TokenOwnerUpdate),
+          data: {
+            txHash: contractEvent.baseParams.txHash,
+            txTimestamp: contractEvent.baseParams.blockTimestamp,
+            status: status,
+            token: {
+              address: _contractEvent.baseParams.address,
+              tokenId: _contractEvent.event.tokenId
+            },
+            owner
+          }
+        };
+        break;
+      }
+      default: {
+        throw new Error(`Unhandled event type ${type}`);
+      }
     }
+
+    logger.log(
+      'indexer',
+      `Validated order ${orderData.metadata.id} - Status ${statusEvent.data.status} - Event ${type}`
+    );
+
+    const statusEventRef = orderRef.collection('orderEvents').doc(statusEvent.metadata.id);
+    await batch.addAsync(statusEventRef, statusEvent, { merge: true });
   }
 }
 
-export async function getOrderStatus(order: Flow.Order): Promise<OrderStatus> {
+export async function getOrderStatus(order: Flow.Order | null): Promise<OrderStatus> {
+  if (!order) {
+    return 'expired';
+  }
   const provider = getProvider(order.chainId.toString() as ChainId);
   const now = Date.now();
   const hasStarted = order.startTime * 1000 > now;

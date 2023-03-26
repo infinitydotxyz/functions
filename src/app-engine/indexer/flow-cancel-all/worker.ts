23e02bdb268b16f386191858cef8edf18eadc828
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-03-26 11:16:59 -0500

    removed lock from event processors - assume only a single worker is running

--- src/app-engine/indexer/flow-cancel-all/worker.ts
+++ src/app-engine/indexer/flow-cancel-all/worker.ts
@@ -23,16 +23,16 @@ export default async function (job: Job<JobData>): Promise<WithTiming<JobResult>
     };
   }
 
-  const key = `flow-cancel-all:lock`;
+  // const key = `flow-cancel-all:lock`;
 
-  await useLock(key, 5000, async (signal) => {
-    try {
-      logger.log(`indexer`, `Acquired lock - Handling cancel all events`);
-      await handleCancelAllEvents(signal);
-    } catch (err) {
-      logger.error('indexer', `Failed to handle cancel all events ${err}`);
-    }
-  });
+  // await useLock(key, 5000, async (signal) => {
+  try {
+    logger.log(`indexer`, `Acquired lock - Handling cancel all events`);
+    await handleCancelAllEvents();
+  } catch (err) {
+    logger.error('indexer', `Failed to handle cancel all events ${err}`);
+  }
+  // });
 
   return {
     id: job.data.id,

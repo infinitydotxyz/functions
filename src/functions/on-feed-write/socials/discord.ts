7b7be4fdbd5e1c451dcade79f193cca038f6a24b
nneverlander <findadit@gmail.com>
2023-03-27 10:56:25 -0500

    minor

--- src/functions/on-feed-write/socials/discord.ts
+++ src/functions/on-feed-write/socials/discord.ts
@@ -119,10 +119,10 @@ export function notifyDiscordWebhook(event: FeedEvent) {
 
   embed.color = 16777215; // white
   embed.timestamp = new Date(event.timestamp).toISOString();
-  // adi-todo: uncomment and replace image embed.footer = {
-  //   text: event.type.split('_').pop() || '',
-  //   icon_url: 'https://pbs.twimg.com/profile_images/1488261914731814915/nyEgvjn2_400x400.png'
-  // };
+  embed.footer = {
+    text: event.type.split('_').pop() || '',
+    icon_url: 'https://pbs.twimg.com/profile_images/1640378608492371969/Vkm_Wevj_400x400.jpg'
+  };
 
   return webhook.send({
     embeds: [embed]

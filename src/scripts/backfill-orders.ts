3a50cdd5318daf6a3a823fe36ae5a35e637ad08e
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2023-05-15 11:45:42 -0500

    added all option to backfill orders script

--- src/scripts/backfill-orders.ts
+++ src/scripts/backfill-orders.ts
@@ -2,6 +2,7 @@ import { ChainId } from '@infinityxyz/lib/types/core';
 import { trimLowerCase } from '@infinityxyz/lib/utils';
 
 import { getDb } from '@/firestore/db';
+import { SupportedCollectionsProvider } from '@/lib/collections/supported-collections-provider';
 import { backfillActiveListings } from '@/lib/reservoir/order-events/backfill-active-orders';
 
 async function main() {
@@ -11,14 +12,25 @@ async function main() {
       'Invalid number of arguments. Usage is: npm run backfill-reservoir-orders <chainId>:<collectionAddress>'
     );
   }
-  const chainId = process.argv[2].split(':')[0];
-  const collectionAddress = process.argv[2].split(':')[1].toLowerCase();
-  if (chainId === '1') {
-    await backfillActiveListings(ChainId.Mainnet, trimLowerCase(collectionAddress), db);
-  } else if (chainId === '5') {
-    await backfillActiveListings(ChainId.Goerli, trimLowerCase(collectionAddress), db);
+
+  if (trimLowerCase(process.argv[2]) === 'all') {
+    const supportedCollections = new SupportedCollectionsProvider(db);
+    await supportedCollections.init();
+
+    for (const item of supportedCollections.values()) {
+      const [chainId, collectionAddress] = item.split(':');
+      await backfillActiveListings(chainId as ChainId, trimLowerCase(collectionAddress), db);
+    }
   } else {
-    throw new Error('Invalid chain id');
+    const chainId = process.argv[2].split(':')[0];
+    const collectionAddress = process.argv[2].split(':')[1].toLowerCase();
+    if (chainId === '1') {
+      await backfillActiveListings(ChainId.Mainnet, trimLowerCase(collectionAddress), db);
+    } else if (chainId === '5') {
+      await backfillActiveListings(ChainId.Goerli, trimLowerCase(collectionAddress), db);
+    } else {
+      throw new Error('Invalid chain id');
+    }
   }
 }
 
